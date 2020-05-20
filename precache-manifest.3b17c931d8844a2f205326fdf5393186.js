b048a2ebaddff832087763d96917bfca033c005d
lavoche <alexlavoche@gmail.com>
2020-05-21 02:40:07 +0300

    Updates

---
+++ precache-manifest.3b17c931d8844a2f205326fdf5393186.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e72e02f121bcaf3535890fcdf3b43a54",
+    "url": "/react-app-map/index.html"
+  },
+  {
+    "revision": "f255d4bb40e87dc28b1a",
+    "url": "/react-app-map/static/css/main.e26ecce2.chunk.css"
+  },
+  {
+    "revision": "9d11fe8f5909f529f2d7",
+    "url": "/react-app-map/static/js/2.19d10ed1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app-map/static/js/2.19d10ed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f255d4bb40e87dc28b1a",
+    "url": "/react-app-map/static/js/main.897a6782.chunk.js"
+  },
+  {
+    "revision": "402b82559454b101f700",
+    "url": "/react-app-map/static/js/runtime-main.7f9c0f50.js"
+  }
+]);
